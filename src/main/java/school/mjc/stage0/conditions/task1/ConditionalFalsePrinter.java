20f43e9dcc9c151d1e31e41945792ff78fe239e5
nasty <anastaishastil2002@gmail.com>
2022-09-04 16:59:48 +0300

    Updated

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (1 > 100) {
+        } else {
+            System.out.println("false");
+        }
     }
 }
